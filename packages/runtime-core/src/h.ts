016458fa341b899440fb23abf1bfdbd5e55a67c8
benjamin <894306909@qq.com>
2023-07-04 23:46:59 +0800

    chore: add vnode

---
+++ packages/runtime-core/src/h.ts
@@ -0,0 +1,5 @@
+import { VNode, createVNode } from "./vnode";
+
+export function h(type: any, propsOrChildren?: any, children?: any): VNode {
+    return createVNode()
+}
---
+++ packages/runtime-core/src/vnode.ts
@@ -0,0 +1,15 @@
+export interface VNode {
+    __v_isVNode: true
+    type: any
+    props: any
+    children: any
+    shapeFlag: number
+}
+
+export function isVNode(value: any): value is VNode {
+    return value ? value.__v_isVNode === true: false
+}
+
+export function createVNode() {
+
+}
