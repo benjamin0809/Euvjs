e532df5e4722348d046544602cb252e91b94c6e2
benjamin <894306909@qq.com>
2023-07-05 23:17:58 +0800

    chore: vnode

--- packages/runtime-core/src/vnode.ts
+++ packages/runtime-core/src/vnode.ts
@@ -1,5 +1,10 @@
 import { baseFlags, isArray, isFunction, isObject, isString } from '@euv/shared'
+import { normalizeClass } from 'packages/shared/src/normalizeProp'
 import { ShapeFlags } from 'packages/shared/src/shapeFlags'
+
+export const Fragment = Symbol('Fragment')
+export const Text = Symbol('Text')
+export const Comment = Symbol('Comment')
 
 export interface VNode {
   [baseFlags.VNODE]: true
@@ -7,6 +12,7 @@ export interface VNode {
   props: any
   children: any
   shapeFlag: number
+  key: any
 }
 
 export function isVNode(value: any): value is VNode {
@@ -21,11 +27,18 @@ export function isVNode(value: any): value is VNode {
  * @returns vnode 对象
  */
 export function createVNode(type, props, children) {
-  const shapeFlag = isString(type) 
-  ? ShapeFlags.ELEMENT 
+  const shapeFlag = isString(type)
+    ? ShapeFlags.ELEMENT
     : isObject(type)
-      ? ShapeFlags.STATEFUL_COMPONENT
-      : 0
+    ? ShapeFlags.STATEFUL_COMPONENT
+    : 0
+
+  if (props) {
+    let { class: klass, style } = props
+    if (klass) {
+      props.class = normalizeClass(klass)
+    }
+  }
   return createBaseVNode(type, props, children, shapeFlag)
 }
 
@@ -66,6 +79,6 @@ export function normalizeChildren(vnode: VNode, children: unknown) {
   vnode.shapeFlag |= type
 }
 
-export const Fragment = Symbol('Fragment')
-export const Text = Symbol('Text')
-export const Comment = Symbol('Comment')
+export function isSameVNodeType(n1: VNode, n2: VNode) {
+  return n1.type === n2.type && n1.key === n2.key
+}
